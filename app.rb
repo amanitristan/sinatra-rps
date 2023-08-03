dae1f4905d18d4900c5c9731a51cebdd3b888569
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-03 02:14:35 +0000

    Complete /rock route code

--- app.rb
+++ app.rb
@@ -16,30 +16,29 @@ end
 
 
 get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  comp_move = moves.sample
+
+  if comp_move == "rock"
+    outcome = "tied"
+  elsif comp_move == "paper"
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
+
   "
-  <h2>We played rock!<h2>
-  <h2>We played rock!<h2>
-  <h2>We played rock!<h2>
-  "
+  <h2>
+  We played rock!
+  <h2>
 
-end
+  <h2>
+  They played #{comp_move}!
+  <h2>
 
-
-get("/paper") do
-  "
-  <h2>We played paper!<h2>
-  <h2>We played ...!<h2>
-  <h2>We played ...!<h2>
-  "
-
-end
-
-
-get("/scissors") do
-  "
-  <h2>We played scissors!<h2>
-  <h2>We played ...!<h2>
-  <h2>We played ...!<h2>
+  <h2>
+  We #{outcome}!
+  <h2>
   "
 
 end
